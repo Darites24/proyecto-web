62a8358ac33bbdaba661761817b67b35bc07bc3c
Darites24 <juanjolopez2402@gmail.com>
2025-05-06 20:28:28 -0500

    Implementación de front en el login

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,8 +3,13 @@ import React from "react";
 
 export default function Dashboard() {
     return(
-        <div>
+        <div className="huge-container">
             <h1>Dashboard</h1>
+            <div className="big-container">
+                <div className="container-saldos"></div>
+                <div className="container-ingresos"></div>
+                <div className="container-egresos"></div>
+            </div>
         </div>
     )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import Dashboard from "./Dashboard";
 import { useNavigate } from "react-router-dom";
 import RegistroUsuario from "./RegistroUsuario";
 import '../styles/Login.css'; 
+import lonelyImg from "../assets/you-look-lonely.jpg";
 
 export default function Login() { //Snippet rfc 
     const [email, setEmail] = useState('');
@@ -18,14 +19,19 @@ export default function Login() { //Snippet rfc
         navigate('/registro');
     }
     return(
-        <div className="container">    
-            <div className="container-login">
-                <h1>Login</h1>
-                <input type="email" placeholder="Correo" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button onClick={handleLogin}>Iniciar Sesion</button>
-                <a onClick={handleRegister} href="">Registrarse</a>
-            </div>
+        <div className="container">  
+                <div className="container-image">
+                    <img src={lonelyImg}></img>
+                </div>  
+            <div className="container-container-login">
+                <div className="container-login">
+                    <h1>Login</h1>
+                    <input type="email" placeholder="Correo" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button onClick={handleLogin}>Iniciar Sesion</button>
+                    <a onClick={handleRegister} href="">Registrarse</a>
+                </div>
+            </div>    
         </div>
     )
 }
